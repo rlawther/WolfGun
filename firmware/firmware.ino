a380606c0fb18547e123a1d7eed4fc1cd18d413e
rlawther <rlawther@cse.unsw.edu.au>
2013-07-21 17:19:46 +1000

    reads both mpu6050 and nunchuck (but only reads nunchuck every couple of seconds)

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -136,6 +136,12 @@ float ypr[3]; // [yaw, pitch, roll] yaw/pitch/roll container and gravity vector
 // packet structure for InvenSense teapot demo
 uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
+#define NUNCHUCK_DEV       0x52
+#define NUNCHUCK_INIT_REG  0x40
+
+uint8_t outbuf[6];		// array to store arduino output
+uint8_t zero = 0x00;
+int cnt = 0;
 
 
 // ================================================================
@@ -160,7 +166,7 @@ void setup() {
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
-    Serial.begin(9600);
+    Serial.begin(57600);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
 
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
@@ -216,9 +222,19 @@ void setup() {
 
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
+    
+    /* Init the nunchuck */
+  I2Cdev::writeBytes(NUNCHUCK_DEV, NUNCHUCK_INIT_REG, 1, &zero);
+    
 }
 
-
+void
+send_zero ()
+{
+  Wire.beginTransmission (0x52);	// transmit to device 0x52
+  Wire.write (0x00);		// sends one byte
+  Wire.endTransmission ();	// stop transmitting
+}
 
 // ================================================================
 // === MAIN PROGRAM LOOP ===
@@ -240,6 +256,24 @@ void loop() {
         // .
         // .
         // .
+        Wire.requestFrom (0x52, 6);	// request data from nunchuck
+        while (Wire.available ())
+          {
+            outbuf[cnt] = nunchuk_decode_byte (Wire.read ());	// receive byte as an integer
+            //outbuf[cnt] = Wire.read ();	// receive byte as an integer
+            //digitalWrite (ledPin, HIGH);	// sets the LED on
+            cnt++;
+          }
+      
+        // If we recieved the 6 bytes, then go print them
+        if (cnt >= 5)
+          {
+            print ();
+          }
+      
+        cnt = 0;
+        send_zero (); // send the request for next bytes
+        delay (100); 
     }
 
     // reset interrupt flag and get INT_STATUS byte
@@ -352,6 +386,96 @@ void loop() {
         // blink LED to indicate activity
         blinkState = !blinkState;
         digitalWrite(LED_PIN, blinkState);
+        //print();
         delay(20);
+        
     }
+}
+
+// Encode data to format that most wiimote drivers except
+// only needed if you use one of the regular wiimote drivers
+char
+nunchuk_decode_byte (char x)
+{
+  x = (x ^ 0x17) + 0x17;
+  return x;
+}
+// Print the input data we have recieved
+// accel data is 10 bits long
+// so we read 8 bits, then we have to add
+// on the last 2 bits.  That is why I
+// multiply them by 2 * 2
+void
+print ()
+{
+  int joy_x_axis = outbuf[0];
+  int joy_y_axis = outbuf[1];
+  int accel_x_axis = outbuf[2] * 2 * 2; 
+  int accel_y_axis = outbuf[3] * 2 * 2;
+  int accel_z_axis = outbuf[4] * 2 * 2;
+
+  int z_button = 0;
+  int c_button = 0;
+
+ // byte outbuf[5] contains bits for z and c buttons
+ // it also contains the least significant bits for the accelerometer data
+ // so we have to check each bit of byte outbuf[5]
+  if ((outbuf[5] >> 0) & 1)
+    {
+      z_button = 1;
+    }
+  if ((outbuf[5] >> 1) & 1)
+    {
+      c_button = 1;
+    }
+
+  if ((outbuf[5] >> 2) & 1)
+    {
+      accel_x_axis += 2;
+    }
+  if ((outbuf[5] >> 3) & 1)
+    {
+      accel_x_axis += 1;
+    }
+
+  if ((outbuf[5] >> 4) & 1)
+    {
+      accel_y_axis += 2;
+    }
+  if ((outbuf[5] >> 5) & 1)
+    {
+      accel_y_axis += 1;
+    }
+
+  if ((outbuf[5] >> 6) & 1)
+    {
+      accel_z_axis += 2;
+    }
+  if ((outbuf[5] >> 7) & 1)
+    {
+      accel_z_axis += 1;
+    }
+
+  Serial.print (joy_x_axis, DEC);
+  Serial.print ("\t");
+
+  Serial.print (joy_y_axis, DEC);
+  Serial.print ("\t");
+
+  Serial.print (accel_x_axis, DEC);
+  Serial.print ("\t");
+
+  Serial.print (accel_y_axis, DEC);
+  Serial.print ("\t");
+
+  Serial.print (accel_z_axis, DEC);
+  Serial.print ("\t");
+
+  Serial.print (z_button, DEC);
+  Serial.print ("\t");
+
+  Serial.print (c_button, DEC);
+  Serial.print ("\t");
+
+  Serial.print ("\r\n");
 }
