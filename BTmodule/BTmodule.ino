b9a33ca57e289096cd3ee44da5226125ececa79b
rlawther <rlawther@gmail.com>
2013-09-06 23:04:54 +1000

    Sets BT device name to Wolfgun

---
+++ BTmodule/BTmodule.ino
@@ -0,0 +1,20 @@
+void setup() { 
+ //Initialize serial and wait for port to open:
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+  
+  // prints title with ending line break 
+  Serial.print("AT");
+  delay(1000);
+  Serial.print("AT");
+  delay(1000);
+  Serial.print("AT");
+  delay(1000);
+  Serial.print("AT+NAMEWolfgun"); 
+} 
+
+void loop()
+{
+}
